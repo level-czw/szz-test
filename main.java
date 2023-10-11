5bb01fa02014efbfbc14563f4dac9ee4863b19b8
czw-level <1374238766@qq.com>
2023-10-11 11:54:09 +0800

    add x

--- main.java
+++ main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String a="[1,4],[2,4],[3,1],[3,2]";
         System.out.println(a.replace('[', '{').replace(']', '}'));
-		int b=1;
-		int c=b*10;
+		int x=10;
     }
 }
