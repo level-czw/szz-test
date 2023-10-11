05cc029ef08e79194543f2d7d1c45dfebe43aaa7
czw-level <1374238766@qq.com>
2023-10-11 11:06:44 +0800

    add \n

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String a="[1,4],[2,4],[3,1],[3,2]";
         System.out.println(a.replace('[', '{').replace(']', '}'));
+		
     }
 }
