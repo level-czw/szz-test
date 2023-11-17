7510d42f7e1079bf3a9a5dd65e4da760ca294bce
czw-level <1374238766@qq.com>
2023-11-17 17:42:23 +0800

    add func

--- a.c
+++ a.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+void print_(char* s){
+	printf(s);
+}
 int main()
 {
    // printf() 中字符串需要引号
    printf("Hello, World!");
-   
-   
-   
-   printf("wdnmd");
+   print_("wdnmd")
    return 0;
 }
